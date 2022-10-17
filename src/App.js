bf56102468ae3d6fc1950abab9f2ce69c1fa3b15
Irene <icasaburi1@gmail.com>
2022-10-17 17:32:43 +0100

    Added footer

--- src/App.js
+++ src/App.js
@@ -9,6 +9,25 @@ export default function App() {
         <div className="row">
           <Weather defaultCity="London" />
         </div>
+        <footer>
+          Created by{" "}
+          <a
+            href="https://www.linkedin.com/in/irenecasaburi/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Irene Casaburi
+          </a>
+          . Open-source code available on{" "}
+          <a
+            href="https://github.com/Irene-Casaburi/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+          .
+        </footer>
       </div>
     </div>
   );
